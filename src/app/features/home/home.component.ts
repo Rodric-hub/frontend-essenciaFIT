f61418c8c938126d8b40634499e9ff29a90d54b9
PieroR <pierordv@gmail.com>
2026-06-23 17:48:57 -0600

    feat: rediseña sección hero con carrusel propio y navbar transparente sobre el home

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,7 +1,7 @@
 // src/app/features/home/home.component.ts
-import { Component, OnInit, signal } from '@angular/core';
+import { Component, OnInit, OnDestroy, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { ProductoService } from '../../core/services/producto.service';
 import { CarritoService } from '../../core/services/carrito.service';
 import { AuthService } from '../../core/services/auth.service';
@@ -11,20 +11,60 @@ import { Producto } from '../../core/models/models';
   selector: 'app-home',
   standalone: true,
   imports: [CommonModule, RouterLink],
-  templateUrl: './home.component.html'
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
   productos = signal<Producto[]>([]);
   msgExito = signal('');
+
+  // Carrusel promocional
+  promoImages = ['/img/promo1.jpg', '/img/promo2.jpg', '/img/promo3.jpg'];
+  currentSlide = 0;
+  private autoplayInterval?: ReturnType<typeof setInterval>;
 
   constructor(
     private productoService: ProductoService,
     private carritoService: CarritoService,
+    private router: Router,
     public auth: AuthService
   ) {}
 
   ngOnInit() {
     this.productoService.getTop8().subscribe(p => this.productos.set(p));
+    this.startAutoplay();
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.autoplayInterval);
+  }
+
+  startAutoplay() {
+    this.autoplayInterval = setInterval(() => this.nextSlide(), 5000);
+  }
+
+  resetAutoplay() {
+    clearInterval(this.autoplayInterval);
+    this.startAutoplay();
+  }
+
+  nextSlide() {
+    this.currentSlide = (this.currentSlide + 1) % this.promoImages.length;
+  }
+
+  prevSlide() {
+    this.currentSlide =
+      (this.currentSlide - 1 + this.promoImages.length) % this.promoImages.length;
+    this.resetAutoplay();
+  }
+
+  goToSlide(i: number) {
+    this.currentSlide = i;
+    this.resetAutoplay();
+  }
+
+  irAProductos() {
+    this.router.navigate(['/productos']);
   }
 
   agregarAlCarrito(productoId: number) {
--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,6 +1,6 @@
 // src/app/shared/navbar/navbar.component.ts
-import { Component, OnInit, signal } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Component, OnInit, signal, HostListener } from '@angular/core';
+import { RouterLink, RouterLinkActive, Router, NavigationEnd } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { AuthService } from '../../core/services/auth.service';
 import { CategoriaService } from '../../core/services/categoria.service';
@@ -10,18 +10,35 @@ import { Categoria } from '../../core/models/models';
   selector: 'app-navbar',
   standalone: true,
   imports: [CommonModule, RouterLink, RouterLinkActive],
-  templateUrl: './navbar.component.html'
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
 })
 export class NavbarComponent implements OnInit {
   categorias = signal<Categoria[]>([]);
+  isHome = signal(true);
+  isScrolled = signal(false);
 
   constructor(
     public auth: AuthService,
-    private categoriaService: CategoriaService
+    private categoriaService: CategoriaService,
+    private router: Router
   ) {}
 
   ngOnInit() {
     this.categoriaService.getAll().subscribe(cats => this.categorias.set(cats));
+
+    this.isHome.set(this.router.url === '/');
+
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.isHome.set(event.url === '/');
+      }
+    });
+  }
+
+  @HostListener('window:scroll')
+  onScroll() {
+    this.isScrolled.set(window.scrollY > 50);
   }
 
   logout() {
