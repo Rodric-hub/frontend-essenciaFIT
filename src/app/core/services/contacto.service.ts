dcc5f22509231fccedddd77e3e8bfc6bdd5a8238
PieroR <pierordv@gmail.com>
2026-06-24 08:27:19 -0600

    fix urls

--- src/app/core/services/contacto.service.ts
+++ src/app/core/services/contacto.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 
 @Injectable({ providedIn: 'root' })
 export class ContactoService {
-  private api = 'http://localhost:8081/api/contacto';
+  private api = 'https://backend-essenciafit.onrender.com/api/contacto';
   constructor(private http: HttpClient) {}
   enviar(data: { nombre: string; email: string; asunto: string; mensaje: string }) {
     return this.http.post(this.api, data);
